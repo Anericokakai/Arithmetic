a0cd45e173205431df4718ca6fe33b39f2e1edde
Brenda Mukami <breshy@localhost.localdomain>
2023-11-07 19:39:12 +0300

    multiplication and division class

---
+++ MATH_COMPUTATIONS/src/main/java/Multiplication.java
@@ -0,0 +1,10 @@
+
+    public class Multiplication{
+
+        //creating a method to carry out multiplication
+        public double multiply(double firstNumber, double secondNumber){
+            return firstNumber * secondNumber;
+        }
+    }
+
+
