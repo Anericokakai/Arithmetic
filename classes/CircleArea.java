2fb864d307ebe6f477954c3c1fb4e8ff102c978b
jamesmukumu <jamesmukumu03@gmail.com>
2023-11-09 19:06:36 +0300

    modified the circle area

--- classes/CircleArea.java
+++ classes/CircleArea.java
@@ -1,9 +1,16 @@
-package classes;
-
-public class CircleArea {
-
-    public double area(int radius ){
-        double PI = (double) 22 / 7;
-        return  PI * radius * radius;
+public class circle {
+    public class Circle {
+        private double radius;
+    
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+    
+        public double area() {
+            return 3.14159 * Math.pow(this.radius, 2);
+        }
     }
-}
+    
+        
+    }
+// modified the area calculation using math.pow    
