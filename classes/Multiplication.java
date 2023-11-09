dbf33e48ac3937e01645287340d96dcd9f7fea31
Brenda Mukami <breshy@localhost.localdomain>
2023-11-09 18:07:45 +0300

    Handled the Division ones more

---
+++ classes/Multiplication.java
@@ -0,0 +1,12 @@
+package classes;
+
+    public class Multiplication{
+
+        //creating a method to carry out multiplication
+        public int multiply(int firstNumber, int secondNumber){
+            return firstNumber * secondNumber;
+        }
+    }
+
+
+
